a652a4f718d286d459209febe899e06cf67d112f
Samuel Silva <samuelmec1@gmail.com>
2022-11-09 22:30:25 -0300

    Abstraindo Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "992158587", modelo: "Tijolão", imei: "555", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo cobrinha");
+
+Console.WriteLine("\nIphone");
+Smartphone iphone = new Iphone(numero: "992156647", modelo: "Iphone 11", imei: "111", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
